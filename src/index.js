179a886c0b4e545f7e14198117fc3ed26036a669
Inna <inna.kachinskaya13@gmail.com>
2021-09-14 23:23:07 +0300

    Add module os

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 "use strict";
 
-//загрузка модуля
-//не нужно деструктурировать
-const MyMath = require("./MyMath");
-const Component = require("./Component.js");
+const os = require("os");
 
-console.log("Hello!");
-
-const header = new Component();
-
-console.log(MyMath.sum(1, 2, 58, 7, 63));
-console.log(MyMath.substract(1, 2, 58, 7, 63));
-console.log(MyMath.div(1, 2, 58, 7, 63));
-console.log(MyMath.multiply(1, 2, 58, 7, 63));
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.totalmem() / (1024 * 1024 * 1024));
